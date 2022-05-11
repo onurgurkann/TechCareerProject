d10a7d58b16d88b3b74792dffaeb46d57acc491b
Onur Gürkan <onurgurkaann@gmail.com>
2022-05-11 20:11:03 +0300

    BaseEntity-AdminEntity-UserEntity created

---
+++ src/main/java/com/onurgurkan/data/entity/BaseEntity.java
@@ -0,0 +1,48 @@
+package com.onurgurkan.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"created_date", "update_date"}, allowGetters = true)
+abstract public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updateBy;
+
+    @LastModifiedDate
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Column(name = "system_created_date", updatable = false)
+    private Date date;
+}
---
+++ src/main/java/com/onurgurkan/data/entity/UserEntity.java
@@ -0,0 +1,4 @@
+package com.onurgurkan.data.entity;
+
+public class UserEntity {
+}
