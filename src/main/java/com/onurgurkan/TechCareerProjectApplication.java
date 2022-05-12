a2cf97d543425f1da7e3b42203620d0d5bc78c1d
Onur Gürkan <onurgurkaann@gmail.com>
2022-05-12 16:45:12 +0300

    @EnableJpaAuditing added

--- src/main/java/com/onurgurkan/TechCareerProjectApplication.java
+++ src/main/java/com/onurgurkan/TechCareerProjectApplication.java
@@ -2,12 +2,14 @@ package com.onurgurkan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 }
 )
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class TechCareerProjectApplication {
 
     public static void main(String[] args) {
