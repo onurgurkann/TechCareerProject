ce02fa7a1a25c81837341550d84ee21eb629ddd4
Onur Gürkan <onurgurkaann@gmail.com>
2022-05-12 15:19:46 +0300

    UserRepository edit

--- src/main/java/com/onurgurkan/data/repository/UserRepository.java
+++ src/main/java/com/onurgurkan/data/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.onurgurkan.data.repository;
 
-import com.onurgurkan.business.dto.UserDto;
+import com.onurgurkan.data.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserDto, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
