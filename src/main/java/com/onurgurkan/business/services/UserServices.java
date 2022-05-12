66b7329904f006d2aa38511869a85e6ebcb6616d
Onur Gürkan <onurgurkaann@gmail.com>
2022-05-12 15:09:17 +0300

    UserServices added

---
+++ src/main/java/com/onurgurkan/business/services/UserServices.java
@@ -0,0 +1,32 @@
+package com.onurgurkan.business.services;
+
+import com.onurgurkan.business.dto.UserDto;
+import com.onurgurkan.data.entity.UserEntity;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserServices {
+
+    // ModelMapper : Dto to Entity - Entity to Dto
+    public UserDto EntityToDto(UserEntity userEntity);
+    public UserEntity DtoToEntity(UserDto userDto);
+
+    // CRUD
+    // Save
+    public UserDto createUser(UserDto userDto);
+
+    // List
+    public List<UserDto> getAllUsers();
+
+    // Find
+    public ResponseEntity<UserDto> getUserById(Long id);
+
+    // Update
+    public ResponseEntity<UserDto> updateUser(Long id, UserDto userDto);
+
+    // Delete
+    public ResponseEntity<Map<String, Boolean>> deleteUser(Long id);
+
+}
