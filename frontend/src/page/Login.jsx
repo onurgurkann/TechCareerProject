30f1768f3d8854d9878dc7d1b3f8641fb993dcb7
Onur Gürkan <onurgurkaann@gmail.com>
2022-05-19 20:44:59 +0300

    Login updated

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { useNavigate, useParams } from 'react-router-dom';
 import './Login.css';
 
-export default class Login extends Component {
+class Login extends Component {
+
+    constructor(props){
+        super(props)
+        this.state={}
+
+        this.cancel = this.cancel.bind(this)
+    }
+
+    cancel(){
+        this.props.navigate('/')
+    }
+
     render() {
         return (
             <>
@@ -10,27 +23,38 @@ export default class Login extends Component {
                     <h2>Giriş Yap</h2>
                     <Form className="form">
                         <FormGroup>
-                            <Label for="exampleEmail">Kullanıcı Adı</Label>
+                            <Label for="username">Kullanıcı Adı</Label>
                             <Input
-                                type="email"
-                                name="email"
-                                id="exampleEmail"
-                                placeholder="abc@abc.com"
+                                type="text"
+                                name="username"
+                                id="username"
+                                placeholder=""
                             />
                         </FormGroup>
                         <FormGroup>
-                            <Label for="examplePassword">Parola</Label>
+                            <Label for="password">Parola</Label>
                             <Input
                                 type="password"
                                 name="password"
-                                id="examplePassword"
-                                placeholder="********"
+                                id="password"
+                                placeholder=""
                             />
                         </FormGroup>
-                        <Button>Giriş Yap</Button>
+                        <Button className='btn btn-success'>Giriş Yap</Button>
+                        <Button className='btn btn-danger' style={{ marginLeft: "2%" }} onClick ={this.cancel}>İptal</Button>
                     </Form>
                 </div>
             </>
         )
     }
 }
+
+export default (props) => (
+    <Login
+      {...props}
+      params={useParams()}
+  
+      navigate={useNavigate()}
+      {...props}
+    />
+  );
