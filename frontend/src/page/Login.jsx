3adbfab7dc4202d20ed202f30f5e788ca1583a4d
Onur Gürkan <onurgurkaann@gmail.com>
2022-05-20 17:55:13 +0300

    Login updated

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -1,48 +1,54 @@
 import React, { Component } from 'react'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 import { useNavigate, useParams } from 'react-router-dom';
-import './Login.css';
 
 class Login extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={}
+        this.state = {}
 
         this.cancel = this.cancel.bind(this)
     }
 
-    cancel(){
+    cancel() {
         this.props.navigate('/')
     }
 
     render() {
         return (
             <>
-                <div className="login">
-                    <h2>Giriş Yap</h2>
-                    <Form className="form">
-                        <FormGroup>
-                            <Label for="username">Kullanıcı Adı</Label>
-                            <Input
-                                type="text"
-                                name="username"
-                                id="username"
-                                placeholder=""
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="password">Parola</Label>
-                            <Input
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder=""
-                            />
-                        </FormGroup>
-                        <Button className='btn btn-success'>Giriş Yap</Button>
-                        <Button className='btn btn-danger' style={{ marginLeft: "2%" }} onClick ={this.cancel}>İptal</Button>
-                    </Form>
+                <br /><br /><br />
+                <div className='container'>
+                    <div className='row'>
+                        <div className='card col-md-6 offset-md-3'><br />
+                            <div className='text-center bg-light'><h3>Giriş Yap</h3></div>
+                            <div className='card-body'>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="username">Kullanıcı Adı</Label>
+                                        <Input
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            placeholder=""
+                                        />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label for="password">Parola</Label>
+                                        <Input
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            placeholder=""
+                                        />
+                                    </FormGroup>
+                                    <Button className='btn btn-success'>Giriş Yap</Button>
+                                    <Button className='btn btn-danger' style={{ marginLeft: "2%" }} onClick={this.cancel}>İptal</Button>
+                                </Form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         )
@@ -51,10 +57,10 @@ class Login extends Component {
 
 export default (props) => (
     <Login
-      {...props}
-      params={useParams()}
-  
-      navigate={useNavigate()}
-      {...props}
+        {...props}
+        params={useParams()}
+
+        navigate={useNavigate()}
+        {...props}
     />
-  );
+);
